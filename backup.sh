bc7bc5088847855477d43dfe8444ea84eaca3917
Bohdan Kalinin <bogdan.kalinin@gmail.com>
2017-06-08 19:53:09 +0300

    Add vars and disable tree

--- backup.sh
+++ backup.sh
@@ -1,39 +1,14 @@
 #!/bin/bash
-##### START #####
-if test -e /media/elektronik/elektroNIK_500GB/.backup/
-then
-  rsync                                         \
-    --archive                                   \
-    --human-readable                            \
-    -PW                                         \
-    --update                                    \
-    --delete-after                              \
-    --delete-excluded                           \
-    --exclude=.*                                \
-    --exclude=*.vdi                             \
-    --exclude=*.avi                             \
-    --exclude=*.mkv                             \
-    --exclude=*.torrent                         \
-    --exclude=*.iso                             \
-    ~                                           \
-    /media/elektronik/elektroNIK_500GB/.backup
-else
-  echo "Не підключено жорсткий диск!"
-  exit
-fi
-if test -e /usr/bin/tree
-then
-  tree -hnRs -o /media/elektronik/elektroNIK_500GB/.backup/All.txt        --dirsfirst --du ~
-  tree -inR  -o /media/elektronik/elektroNIK_500GB/.backup/Soundslist.txt --dirsfirst      ~/Sounds/
-  tree -inR  -o /media/elektronik/elektroNIK_500GB/.backup/Videoslist.txt --dirsfirst      ~/Videos/
-  tree -inR  -o /media/elektronik/elektroNIK_500GB/.backup/Driverlist.txt --dirsfirst      ~/Programs/Drivers/
-  tree -inR  -o /media/elektronik/elektroNIK_500GB/.backup/OSlist.txt     --dirsfirst      ~/Programs/OS/
-  tree -inR  -o /media/elektronik/elektroNIK_500GB/.backup/Softlist.txt   --dirsfirst      ~/Programs/Soft/
-else
-  echo "Не встановлено tree"
-  #kdialog --title "Помилка!" --error "tree не встановлено! \nРезервне копіювання скасовано!"
-exit
-fi
-echo " "
-echo "###ГОТОВО###"
+
+SOURCE="/home/elektronik"
+DEST="elektronik@192.168.0.141:/media/elektroNIK_500GB/.backup"
+
+rsync -P --archive --human-readable --partial --update --delete-during --delete-excluded --exclude-from=backup.exclude $SOURCE $DEST
+
+# tree -hnRs -o $DEST/All.txt        --dirsfirst --du ~
+# tree -inR  -o $DEST/Soundslist.txt --dirsfirst      ~/Sounds/
+# tree -inR  -o $DEST/Videoslist.txt --dirsfirst      ~/Videos/
+# tree -inR  -o $DEST/Driverlist.txt --dirsfirst      ~/Programs/Drivers/
+# tree -inR  -o $DEST/OSlist.txt     --dirsfirst      ~/Programs/OS/
+# tree -inR  -o $DEST/Softlist.txt   --dirsfirst      ~/Programs/Soft/
 exit
